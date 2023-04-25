7fbb2a2a7cf286d4b89e4c97d0e5e0623cbc9c76
Ernest Gomis Ventura <gxmis791@gmail.com>
2023-04-25 19:08:40 +0200

    update de la tabla de usuarios

--- SQL/usuarios.sql
+++ SQL/usuarios.sql
@@ -1,17 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 25-04-2023 a las 19:05:39
+-- Versión del servidor: 10.4.11-MariaDB
+-- Versión de PHP: 7.3.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bbdd-iradareye`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
 
 CREATE TABLE `usuarios` (
   `id_usuario` int(11) NOT NULL,
-  `nombre_usuario` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `correo` varchar(255) DEFAULT NULL,
+  `nombre_usuario` varchar(255) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `correo` varchar(255) COLLATE utf8_spanish_ci DEFAULT NULL,
   `fecha_nac` date DEFAULT NULL,
-  `direccion` varchar(255) DEFAULT NULL,
-  `telefono` varchar(20) DEFAULT NULL,
+  `direccion` varchar(255) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `telefono` varchar(20) COLLATE utf8_spanish_ci DEFAULT NULL,
   `fecha_registro` date DEFAULT NULL,
   `escaperoomsJugadas` int(11) NOT NULL,
-  `escaperoomsGanadas` int(11) NOT NULL
+  `escaperoomsGanadas` int(11) NOT NULL,
+  `tiempo_medio` int(100) DEFAULT NULL,
+  `pais` int(100) DEFAULT NULL,
+  `comunidad` int(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `usuarios`
+--
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id_usuario`);
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
