9c590f17098ebbf22e2d97e9ca8191e088751f51
Ernest Gomis Ventura <gxmis791@gmail.com>
2023-04-26 17:17:22 +0200

    tablas actualizadas

--- SQL/stats.sql
+++ SQL/stats.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 26-04-2023 a las 17:14:53
+-- Tiempo de generación: 26-04-2023 a las 17:12:35
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -18,32 +18,33 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `bbdd-iradareye_users`
+-- Base de datos: `bbdd-iradareye_stats_player`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `usuarios`
+-- Estructura de tabla para la tabla `stats`
 --
 
-CREATE TABLE `usuarios` (
-  `id_usuario` int(11) NOT NULL,
-  `nombre_usuario` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `correo` varchar(255) DEFAULT NULL,
-  `id_stats` int(10) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+CREATE TABLE `stats` (
+  `id_stats` int(10) NOT NULL,
+  `escaperoomsJugadas` int(3) DEFAULT NULL,
+  `escaperoomsGanadas` int(3) DEFAULT NULL,
+  `tiempo_medio` int(10) DEFAULT NULL,
+  `pais` int(3) DEFAULT NULL,
+  `comunidad` int(3) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `usuarios`
+-- Indices de la tabla `stats`
 --
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id_usuario`);
+ALTER TABLE `stats`
+  ADD PRIMARY KEY (`id_stats`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
