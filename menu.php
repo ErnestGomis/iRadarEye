e2dd8cdb7ff00713be4ffab88ea1653f3b57b6c4
Huertas04 <huertas.10401@palcam.cat>
2023-04-28 15:25:18 +0200

    vamos a morir
    lolilolilolo

--- menu.php
+++ menu.php
@@ -1,12 +1,24 @@
 <header>
-<nav class="navbar navbar-dark bg-dark">
+    <nav class="navbar navbar-dark bg-dark">
         <a class="navbar-brand" href="index.php">IradarEye</a>
-        <ul class="navbar-nav ml-auto">
+        <ul class="d-flex">
             <li class="nav-item">
                 <a class="nav-link" href="./sesion/login.php">Login</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="./sesion/register.php">Register</a>
+            </li> 
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Mi Cuenta
+                </a>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Perfil de usuario</a>
+                    <a class="dropdown-item" href="#">Mis escapesrooms</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="./sesion/logout.php">Logout</a>
+                    <a class="dropdown-item" href="#">Configuración</a>
+                </div>
             </li>
         </ul>
     </nav>
